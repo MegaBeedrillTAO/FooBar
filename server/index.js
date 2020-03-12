b9d9367913b08fea856e6e5d5ed712973d82354a
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-03-11 22:17:40 -0500

    Setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const session = require('express-session');
+require('dotenv').config();
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
+
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(dbInst => {
+    app.set("db", dbInst);
+    console.log('Connected to Database');
+})
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
+
+app.listen(SERVER_PORT, () =>{
+    console.log(`Listening on port ${SERVER_PORT}`);
+})
