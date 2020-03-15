80c853fe581a08f6775163202aecaf6d0626a15c
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-03-15 15:23:35 -0500

    Working on fetching

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+
+
 
 
 
 function getNums() {
     let numArea = document.getElementById("content")
-    let nums = [1, 2, 3]
-     axios.get('/api/nums').then(response => {
-        nums = [...response]
+    let nums = []
+    let myRequest = new Request('https://localhost:3003/api/nums', {method: "GET"})
+    fetch(myRequest).then(response => {
+        console.log(response)
     })
     numArea.innerText = nums.join(' ');
     console.log(nums);
